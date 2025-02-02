cbe493e834fd64637029c707936cb2fad91f73b4
alamshahriar <alamshahriar@gmail.com>
2025-02-02 13:32:11 +0600

    add img

--- js/home.js
+++ js/home.js
@@ -1,1 +1,16 @@
-console.log('home added');
+// add money button
+
+document.getElementById('btn-add-money').addEventListener('click', function (event) {
+    event.preventDefault();
+
+    //  step-2: get money to be added to the account
+
+    const addMoneyInput = document.getElementById('input-add-money').value;
+    console.log(addMoneyInput);
+
+    // get the pin number provided
+    const pinNumberInput = document.getElementById('input-pin-number').value;
+    console.log(pinNumberInput);
+
+
+})
